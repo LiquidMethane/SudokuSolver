a5c82ba281444aa2817b70b34b0ee18ffc1a5815
Tian <Tian@10.194.16.70>
2017-05-01 09:48:03 -0600

    solnode

--- SudokuSolver/src/codes/LinkedList.java
+++ SudokuSolver/src/codes/LinkedList.java
@@ -2,4 +2,52 @@ package codes;
 
 public class LinkedList {
 
+	SolNode first;
+	SolNode last;
+	
+	public SolNode getFirst() {
+		return first;
+	}
+	public void setFirst(SolNode first) {
+		this.first = first;
+	}
+	public SolNode getLast() {
+		return last;
+	}
+	public void setLast(SolNode last) {
+		this.last = last;
+	}
+	
+	public LinkedList() {
+		first = null;
+		last = null;
+	}
+	
+	public void push(int grid[][]) {
+		SolNode newNode = new SolNode(grid);
+		if (first == null)
+			first = newNode;
+		else {
+			last.setNext(newNode);
+			newNode.setPrev(last);
+		}
+		last = newNode;
+	}
+	
+	public void pop() {
+		if (last == null) {
+		} else if (last == first) {
+			first = null;
+			last = null;
+		} else {
+			last = last.getPrev();
+			last.setNext(null);
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
--- SudokuSolver/src/codes/SolNode.java
+++ SudokuSolver/src/codes/SolNode.java
@@ -1,5 +1,38 @@
 package codes;
 
 public class SolNode {
+	
+	private int grid[][];
+	private SolNode prev;
+	private SolNode next;
+	
+	public SolNode() {
+		prev = null;
+		next = null;
+	}
+	
+	public SolNode(int grid[][]) {
+		this.grid = grid;
+		prev = null;
+		next = null;
+	}
+	
+	public int[][] getGrid() {
+		return grid;
+	}
+	
+	public SolNode getPrev() {
+		return prev;
+	}
+	public void setPrev(SolNode prev) {
+		this.prev = prev;
+	}
+	public SolNode getNext() {
+		return next;
+	}
+	public void setNext(SolNode next) {
+		this.next = next;
+	}
 
+	
 }
